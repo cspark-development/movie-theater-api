1601813b98fa7df534acc2e10226d2b44c5510b4
cspark <git@cspark.dev>
2024-11-08 16:02:04 +0000

    Init server and modify eslint

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
-const Show = require('./Show')
-const User = require('./User')
+const Show = require("./Show");
+const User = require("./User");
 
-Show.belongsToMany(User, { through: 'watched' })
-User.belongsToMany(Show, { through: 'watched' })
+Show.belongsToMany(User, { through: "watched" });
+User.belongsToMany(Show, { through: "watched" });
 
 module.exports = {
-  Show,
-  User
-}
+	Show,
+	User
+};
