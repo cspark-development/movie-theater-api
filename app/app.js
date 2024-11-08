826f74f4bf5051863c68b7d1a48862dca69d38f5
cspark <git@cspark.dev>
2024-11-08 16:52:26 +0000

    Add middleware

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
+const express = require("express");
 const showsRouter = require("./routes/shows.js");
 const usersRouter = require("./routes/users.js");
 const app = require("./server.js");
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.bind("/users", usersRouter);
 app.bind("/shows", showsRouter);
